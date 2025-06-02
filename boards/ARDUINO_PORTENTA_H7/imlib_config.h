87033c3d9bb61163546ae32018c9a40a07a1e011
sdcnku <sdcnku@gmail.com>
2025-06-02 16:20:49 +1000

    Update imlib_config.h

--- boards/ARDUINO_PORTENTA_H7/imlib_config.h
+++ boards/ARDUINO_PORTENTA_H7/imlib_config.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the OpenMV project.
+ * Thdis file is part of the OpenMV project.
  *
  * Copyright (c) 2013-2021 Ibrahim Abdelkader <iabdalkader@openmv.io>
  * Copyright (c) 2013-2021 Kwabena W. Agyeman <kwagyeman@openmv.io>
